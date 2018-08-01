0b3c4bb7fa41b04fde610e4ff1171d5f21fb5d59
aditibagchi <aditi.bagchi@vai.org>
2018-08-01 13:46:49 -0400

    Commit August_1_2018

---
+++ August_1_18.R
@@ -0,0 +1,46 @@
+SUbset_Mets_Germline <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_1.csv")
+View(SUbset_Mets_Germline_1)
+SUbset_Mets_Germline_1 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_1.csv")
+View(SUbset_Mets_Germline_1)
+DF_1 <- as.data.frame(SUbset_Mets_Germline_1)
+mut.to.sigs.input(mut.ref = DF_1, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_2 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_1.csv")
+View(SUbset_Mets_Germline_2)
+DF_2 <- as.data.frame(SUbset_Mets_Germline_2)
+mut.to.sigs.input(mut.ref = DF_2, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_3 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_2.csv")
+View(SUbset_Mets_Germline_3)
+DF_3 <- as.data.frame(SUbset_Mets_Germline_3)
+mut.to.sigs.input(mut.ref = DF_3, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_4 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_4.csv")
+View(SUbset_Mets_Germline_3)
+DF_4 <- as.data.frame(SUbset_Mets_Germline_4)
+mut.to.sigs.input(mut.ref = DF_4, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_5 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_5.csv")
+View(SUbset_Mets_Germline_5)
+DF_5 <- as.data.frame(SUbset_Mets_Germline_5)
+mut.to.sigs.input(mut.ref = DF_5, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_6 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_5.csv")
+View(SUbset_Mets_Germline_6)
+DF_6 <- as.data.frame(SUbset_Mets_Germline_6)
+mut.to.sigs.input(mut.ref = DF_6, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_6 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_6.csv")
+View(SUbset_Mets_Germline_6)
+DF_6 <- as.data.frame(SUbset_Mets_Germline_6)
+mut.to.sigs.input(mut.ref = DF_6, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_7 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_7.csv")
+View(SUbset_Mets_Germline_7)
+DF_7<- as.data.frame(SUbset_Mets_Germline_7)
+mut.to.sigs.input(mut.ref = DF_7, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_8 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_8.csv")
+View(SUbset_Mets_Germline_7)
+DF_8<- as.data.frame(SUbset_Mets_Germline_8)
+mut.to.sigs.input(mut.ref = DF_8, sample.id = "Sample", chr = "chr", pos = "X",
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # July_December_2018
+August_1 
+Started the analysis of Metstatic recurrent samples from ICGC (Germline)
+Used BSGenome38 and the first error was at Chromosome 2 when switced to HG19 the error was further down (Ch10)
+This is a similar problem we had before. 
