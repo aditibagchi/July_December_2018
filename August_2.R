710af21dd95bc36d7b13582027eb823d5d6de144
aditibagchi <aditi.bagchi@vai.org>
2018-08-02 11:51:46 -0400

    first commit 2nd august

---
+++ August_2.R
@@ -0,0 +1,62 @@
+SUbset_Mets_Germline_9 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_9.csv")
+View(SUbset_Mets_Germline_9)
+DF_9<- as.data.frame(SUbset_Mets_Germline_9)
+mut.to.sigs.input(mut.ref = DF_9, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_10 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_9.csv")
+View(SUbset_Mets_Germline_10)
+DF_10<- as.data.frame(SUbset_Mets_Germline_10)
+mut.to.sigs.input(mut.ref = DF_10, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_10 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_10.csv")
+View(SUbset_Mets_Germline_10)
+DF_10<- as.data.frame(SUbset_Mets_Germline_10)
+mut.to.sigs.input(mut.ref = DF_10, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_11 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_11.csv")
+View(SUbset_Mets_Germline_11)
+DF_11<- as.data.frame(SUbset_Mets_Germline_11)
+mut.to.sigs.input(mut.ref = DF_11, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_12 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_12.csv")
+View(SUbset_Mets_Germline_12)
+mut.to.sigs.input(mut.ref = DF_12, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+DF_12<- as.data.frame(SUbset_Mets_Germline_12)
+mut.to.sigs.input(mut.ref = DF_12, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_13 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_13.csv")
+View(SUbset_Mets_Germline_13)
+DF_13<- as.data.frame(SUbset_Mets_Germline_13)
+mut.to.sigs.input(mut.ref = DF_13, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_14 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_14.csv")
+View(SUbset_Mets_Germline_12)
+DF_14<- as.data.frame(SUbset_Mets_Germline_14)
+mut.to.sigs.input(mut.ref = DF_14, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_15 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_15.csv")
+View(SUbset_Mets_Germline_15)
+DF_15<- as.data.frame(SUbset_Mets_Germline_15)
+mut.to.sigs.input(mut.ref = DF_15, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_16 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_16.csv")
+View(SUbset_Mets_Germline_15)
+DF_16<- as.data.frame(SUbset_Mets_Germline_16)
+mut.to.sigs.input(mut.ref = DF_16, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_17 <- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_17.csv")
+View(SUbset_Mets_Germline_17)
+DF_17<- as.data.frame(SUbset_Mets_Germline_17)
+mut.to.sigs.input(mut.ref = DF_17, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_18<- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_18.csv")
+View(SUbset_Mets_Germline_18)
+mut.to.sigs.input(mut.ref = DF_18, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+DF_18<- as.data.frame(SUbset_Mets_Germline_18)
+mut.to.sigs.input(mut.ref = DF_18, sample.id = "Sample", chr = "chr", pos = "X",
+ref = "ref", alt = "alt", bsg = BSgenome.Hsapiens.UCSC.hg19)
+SUbset_Mets_Germline_19<- read.csv("~/Desktop/Data_Analysis/last MAF files/SUbset_Mets_Germline_19.csv")
+View(SUbset_Mets_Germline_18)
+DF_19<- as.data.frame(SUbset_Mets_Germline_19)
